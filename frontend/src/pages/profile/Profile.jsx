f29fe4b5564fd14e3ddbaa78d4bfe8874330d8a8
Fernanda Vaz <flauta.fernanda@gmail.com>
2025-01-13 12:02:40 -0300

    página de perfil pronta

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,97 +1,55 @@
 import styles from './Profile.module.css'
-import AuthServices from '../../services/auth.jsx'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
-import { Button } from '@mui/material'
-import OrderServices from '../../services/order.jsx'
-import Loading from '../../components/loading/Loading.jsx'
-import MyButton from '../../components/buttons/MyButton.jsx'
+import MyButton from '../../components/buttons/MyButton'
 
 export default function Profile() {
-  const { logout } = AuthServices()
-  const navigate = useNavigate()
-  const authData = JSON.parse(localStorage.getItem('auth'))
-  const { getUserOrders, orderLoading, refetchOrders, ordersList } =
-    OrderServices()
-
-  useEffect(() => {
-    if (!authData) {
-      return navigate('/auth')
-    } else if (refetchOrders) {
-      getUserOrders(authData?.user?._id)
-    }
-  }, [authData, refetchOrders])
-
-  if (orderLoading) {
-    return <Loading />
-  }
-
-  const handleLogout = () => {
-    logout()
-    return navigate('/')
-  }
-
-  console.log(ordersList)
-
   return (
     <div className={styles.pageContainer}>
-      <div className={styles.profileContainer}>
-        <h1>Olá, {authData?.user?.fullname}</h1>
-        <h4>{authData?.user?.email}</h4>
-        <Button color='inherit' onClick={handleLogout}>
-          Logout
-          <img src='/imgs/icons/log-out.svg' alt='' />
-        </Button>
+      <nav>
+        <div className={styles.mobileNavbarItems}>
+          <img
+            className={styles.navbarLink}
+            src='/imgs/icons/arrow-left.svg'
+            alt=''
+          />
+          <h3>Profile</h3>
+          <img src='/imgs/icons/home.svg' alt='' />
+        </div>
+      </nav>
+
+      <div className={styles.profileHeader}>
+        <img src='/imgs/icons/user.svg' alt='' />
+        <div className={styles.profileInfos}>
+          <h3>Usuário Teste</h3>
+          <p>teste@email.com</p>
+        </div>
       </div>
-
-      {ordersList.length > 0 ? (
-        <div className={styles.ordersContainer}>
-          {ordersList.map((order) => (
-            <div key={order._id} className={styles.orderContainer}>
-              {order.pickupStatus === 'Pending' ? (
-                <p className={`${styles.pickupStatus} ${styles.pending}`}>
-                  <img
-                    className={styles.icon}
-                    src='/imgs/icons/clock.svg'
-                    alt=''
-                  />{' '}
-                  Pendente
-                </p>
-              ) : null}
-
-              {order.pickupStatus === 'Completed' ? (
-                <p className={`${styles.pickupStatus} ${styles.completed}`}>
-                  <img src='/imgs/icons/check-square.svg' alt='' /> Finalizado
-                </p>
-              ) : null}
-
-              {order.pickupStatus === 'Canceled' ? (
-                <p className={`${styles.pickupStatus} ${styles.canceled}`}>
-                  <img src='/imgs/icons/x-circle.svg' alt='' /> Cancelado
-                </p>
-              ) : null}
-
-              {order.orderItems.map((item) => (
-                <div key={item._id}>
-                  <h4>{item.itemDetails[0].name}</h4>
-                  <p>Quantidade: {item.quantity}</p>
-                </div>
-              ))}
-            </div>
-          ))}
+      <hr />
+      <div className={styles.pageContent}>
+        <div className={styles.contentItems}>
+          <h4>General</h4>
+          <h3>Edit Profile</h3>
+          <hr />
+          <h3>Notificatios</h3>
+          <hr />
+          <h3>Wishlist</h3>
+          <hr />
         </div>
-      ) : (
-        <div className={styles.notOrdersContainer}>
-            <div className={styles.notOrderText}>
-                <h2>Você não possui pedidos ainda.</h2>
-                <img src='/imgs/icons/sad-face.svg' alt='' />
-            </div>
-          <Button>
-            <Link to={'/products'}>Clique aqui para fazer seu pedido!</Link>
-          </Button>
+
+        <div className={styles.contentItems}>
+          <h4>Legal</h4>
+          <h3>Terms of Use</h3>
+          <hr />
+          <h3>Privacy Policy</h3>
+          <hr />
         </div>
-      )}
+
+        <div className={styles.contentItems}>
+          <h4>Personal</h4>
+          <h3>Report a Bug</h3>
+          <hr />
+          <MyButton>Logout</MyButton>
+        </div>
+      </div>
     </div>
   )
 }
